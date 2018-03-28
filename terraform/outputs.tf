d9e19f2076228e9e789509f87c1506cda120bd05
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2018-03-28 17:26:27 -0400

    Output RPC port in terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,8 @@
 output "eximchain_node_dns" {
   value = "${aws_instance.eximchain_node.public_dns}"
 }
+
+output "eximchain_node_rpc_port" {
+  # TODO: Make this not hard-coded
+  value = "22000"
+}
