7fbab55865e952ea6a7a9bdaa4fcad9e2afe319c
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2018-04-05 11:24:19 -0400

    Use testnet data for testnet 2

--- packer/instance-scripts/init-eximchain-node.sh
+++ packer/instance-scripts/init-eximchain-node.sh
@@ -13,9 +13,10 @@ function wait_for_successful_command {
 }
 
 function download_chain_metadata {
-  curl https://raw.githubusercontent.com/Eximchain/eximchain-testnet-data/master/quorum/testnet-1/quorum-genesis.json > /opt/quorum/private/quorum-genesis.json
-  curl https://raw.githubusercontent.com/Eximchain/eximchain-testnet-data/master/quorum/testnet-1/bootnodes.txt > /opt/quorum/info/bootnodes.txt
-  curl https://raw.githubusercontent.com/Eximchain/eximchain-testnet-data/master/quorum/testnet-1/constellation-bootnodes.txt > /opt/quorum/info/constellation-bootnodes.txt
+  local readonly DATADIR="quorum/testnet-2"
+  curl https://raw.githubusercontent.com/Eximchain/eximchain-testnet-data/master/$DATADIR/quorum-genesis.json > /opt/quorum/private/quorum-genesis.json
+  curl https://raw.githubusercontent.com/Eximchain/eximchain-testnet-data/master/$DATADIR/bootnodes.txt > /opt/quorum/info/bootnodes.txt
+  curl https://raw.githubusercontent.com/Eximchain/eximchain-testnet-data/master/$DATADIR/constellation-bootnodes.txt > /opt/quorum/info/constellation-bootnodes.txt
 }
 
 function generate_eximchain_supervisor_config {
