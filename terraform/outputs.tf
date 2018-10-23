30a4234026066083f5d8ba27541c434d2423ca9d
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2018-10-23 16:38:55 -0400

    Pass hard-coded output from module

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,8 +3,7 @@ output "eximchain_node_dns" {
 }
 
 output "eximchain_node_rpc_port" {
-  # TODO: Make this not hard-coded
-  value = "22000"
+  value = "${module.eximchain_node.eximchain_node_rpc_port}"
 }
 
 output "eximchain_node_iam_role" {
