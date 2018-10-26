b820f698ce4507192d7d10cccd9c42833f83047f
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2018-10-26 12:35:26 -0400

    allow-rpc-rule module can allow access to lb as well

--- terraform/modules/allow-rpc-rule/main.tf
+++ terraform/modules/allow-rpc-rule/main.tf
@@ -1,5 +1,18 @@
-resource "aws_security_group_rule" "rpc_external" {
+resource "aws_security_group_rule" "rpc_external_node" {
   security_group_id = "${var.node_security_group}"
+  type              = "ingress"
+
+  from_port = "${var.rpc_port}"
+  to_port   = "${var.rpc_port}"
+  protocol  = "tcp"
+
+  source_security_group_id = "${var.rpc_security_group}"
+}
+
+resource "aws_security_group_rule" "rpc_external_lb" {
+  count = "${var.using_lb ? 1 : 0}"
+  
+  security_group_id = "${var.lb_security_group}"
   type              = "ingress"
 
   from_port = "${var.rpc_port}"
--- terraform/modules/allow-rpc-rule/variables.tf
+++ terraform/modules/allow-rpc-rule/variables.tf
@@ -6,6 +6,16 @@ variable "rpc_security_group" {
   description = "Security group to grant RPC access to"
 }
 
+variable "using_lb" {
+  description = "Whether a load balancer is being used"
+  default     = false
+}
+
+variable "lb_security_group" {
+  description = "Security group running the load balancer to grant RPC access to"
+  default     = ""
+}
+
 variable "rpc_port" {
   description = "The port the RPC server is available on"
   default     = 22000
