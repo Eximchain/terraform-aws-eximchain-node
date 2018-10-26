18f0b7ca55ec8ea3ddd2f4f831d9ceff82f70dd1
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2018-10-26 12:41:44 -0400

    Fix allow-rpc-rule outputs

--- terraform/modules/allow-rpc-rule/outputs.tf
+++ terraform/modules/allow-rpc-rule/outputs.tf
@@ -1,3 +1,7 @@
-output "security_group_rule_id" {
-  value = "${aws_security_group_rule.rpc_external.id}"
+output "node_security_group_rule_id" {
+  value = "${aws_security_group_rule.rpc_external_node.id}"
+}
+
+output "lb_security_group_rule_id" {
+  value = "${aws_security_group_rule.rpc_external_lb.id}"
 }
