9dcc90b23fb29e137dbe9b2e2d14ec4e3154ca54
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2018-10-26 14:16:45 -0400

    Workaround for allow-rpc-rule not allowing access to load balancer

--- terraform/modules/allow-rpc-rule/main.tf
+++ terraform/modules/allow-rpc-rule/main.tf
@@ -11,7 +11,7 @@ resource "aws_security_group_rule" "rpc_external_node" {
 
 resource "aws_security_group_rule" "rpc_external_lb" {
   count = "${var.using_lb ? 1 : 0}"
-  
+
   security_group_id = "${var.lb_security_group}"
   type              = "ingress"
 
@@ -20,4 +20,18 @@ resource "aws_security_group_rule" "rpc_external_lb" {
   protocol  = "tcp"
 
   source_security_group_id = "${var.rpc_security_group}"
+}
+
+# TODO: Troubleshoot why the above rule isn't allowing access from the tx executor
+resource "aws_security_group_rule" "rpc_lb_allow_all" {
+  count = "${var.using_lb ? 1 : 0}"
+
+  security_group_id = "${var.lb_security_group}"
+  type              = "ingress"
+
+  from_port = "${var.rpc_port}"
+  to_port   = "${var.rpc_port}"
+  protocol  = "tcp"
+
+  cidr_blocks = ["0.0.0.0/0"]
 }
