36114f81bf6f3a6a41635f40b0c69018eb315a25
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2018-10-29 15:47:56 -0400

    Remove "allow all" rule from allow-rpc-rule

--- terraform/modules/allow-rpc-rule/main.tf
+++ terraform/modules/allow-rpc-rule/main.tf
@@ -9,6 +9,7 @@ resource "aws_security_group_rule" "rpc_external_node" {
   source_security_group_id = "${var.rpc_security_group}"
 }
 
+# Load balancer must be internal for this to work
 resource "aws_security_group_rule" "rpc_external_lb" {
   count = "${var.using_lb ? 1 : 0}"
 
@@ -20,18 +21,4 @@ resource "aws_security_group_rule" "rpc_external_lb" {
   protocol  = "tcp"
 
   source_security_group_id = "${var.rpc_security_group}"
-}
-
-# TODO: Troubleshoot why the above rule isn't allowing access from the tx executor
-resource "aws_security_group_rule" "rpc_lb_allow_all" {
-  count = "${var.using_lb ? 1 : 0}"
-
-  security_group_id = "${var.lb_security_group}"
-  type              = "ingress"
-
-  from_port = "${var.rpc_port}"
-  to_port   = "${var.rpc_port}"
-  protocol  = "tcp"
-
-  cidr_blocks = ["0.0.0.0/0"]
 }
