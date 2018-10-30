5f46564db710f991ef1336c7835b70fd13c35254
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2018-10-30 11:12:24 -0400

    Remove obsolete allow-rpc-rule module

--- terraform/modules/allow-rpc-rule/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "node_security_group_rule_id" {
-  value = "${aws_security_group_rule.rpc_external_node.id}"
-}
-
-output "lb_security_group_rule_id" {
-  value = "${element(coalescelist(aws_security_group_rule.rpc_external_lb.*.id, list("")), 0)}"
-}
